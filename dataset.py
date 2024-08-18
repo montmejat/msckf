c0518e99391fb7c07b7774d1cbe6cdf8e8496127
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2024-08-18 23:37:01 +0200

    Dataset iterator return tuple

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import csv
 from dataclasses import dataclass
+from typing import Tuple
 
 import numpy as np
+import yaml
 from PIL import Image
 
 
@@ -45,7 +47,12 @@ class TumDataset:
         self.cam_idx = 0
         self.timestamp = 0
 
-    def __next__(self):
+        with open(f"{folder_path}/dso/imu_config.yaml") as f:
+            imu_config = yaml.safe_load(f)
+            self.gyroscope_noise_density = imu_config["gyroscope_noise_density"]
+            self.imu_sampling_frequency = imu_config["update_rate"]
+
+    def __next__(self) -> Tuple[ImuData, CameraData]:
         def next_camera_data():
             cam_data = self.cam[self.cam_idx]
             self.timestamp = cam_data["timestamp"]
@@ -62,15 +69,17 @@ class TumDataset:
 
         if self.imu_idx < len(self.imu) and self.cam_idx < len(self.cam):
             if self.imu[self.imu_idx].timestamp < self.cam[self.cam_idx]["timestamp"]:
-                return next_imu_data()
+                return (next_imu_data(), None)
+            elif self.imu[self.imu_idx].timestamp > self.cam[self.cam_idx]["timestamp"]:
+                return (None, next_camera_data())
             else:
-                return next_camera_data()
+                return (next_imu_data(), next_camera_data())
         elif self.imu_idx < len(self.imu):
-            return next_imu_data()
+            return (next_imu_data(), None)
         elif self.cam_idx < len(self.cam):
-            return next_camera_data()
-        else:
-            raise StopIteration
+            return (None, next_camera_data())
+
+        raise StopIteration
 
     def __iter__(self):
         return self
