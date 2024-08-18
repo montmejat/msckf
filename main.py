eb5d36c7b27b2c2791c7e1560d8cb34268ca13ec
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2024-08-18 23:38:31 +0200

    MSCKF gyro variance setup

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ import argparse
 import rerun as rr
 
 from dataset import CameraData, ImuData, TumDataset
+from msckf import MSCKF
+
+
+def log_imu(imu_data: ImuData):
+    if imu_data is not None:
+        rr.log("imu/wx", rr.Scalar(imu_data.wx))
+        rr.log("imu/wy", rr.Scalar(imu_data.wy))
+        rr.log("imu/wz", rr.Scalar(imu_data.wz))
+        rr.log("imu/ax", rr.Scalar(imu_data.ax))
+        rr.log("imu/ay", rr.Scalar(imu_data.ay))
+        rr.log("imu/az", rr.Scalar(imu_data.az))
+
+
+def log_camera(camera_data: CameraData):
+    if camera_data is not None:
+        rr.log("camera", rr.Image(camera_data.image))
+
+
+def setup_msckf(dataset: TumDataset) -> MSCKF:
+    gyro_var = dataset.gyroscope_noise_density**2 * dataset.imu_sampling_frequency
+
+    return MSCKF(
+        # Note: (rad/s)/√Hz = rad/s)*√s
+        # ((rad/s)/√Hz)**2 * Hz = rad**2 / s**2
+        [gyro_var] * 3,
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,18 +37,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset = TumDataset(args.dataset)
+    msckf = setup_msckf(dataset)
 
     rr.init("MSCKF", spawn=True)
 
-    for data in dataset:
-        rr.set_time_nanos("sensors", data.timestamp)
+    i = 0
+    for imu_data, camera_data in dataset:
+        rr.set_time_nanos("sensors", dataset.timestamp)
+        log_imu(imu_data)
+        log_camera(camera_data)
 
-        if isinstance(data, CameraData):
-            rr.log("camera", rr.Image(data.image))
-        elif isinstance(data, ImuData):
-            rr.log("imu/wx", rr.Scalar(data.wx))
-            rr.log("imu/wy", rr.Scalar(data.wy))
-            rr.log("imu/wz", rr.Scalar(data.wz))
-            rr.log("imu/ax", rr.Scalar(data.ax))
-            rr.log("imu/ay", rr.Scalar(data.ay))
-            rr.log("imu/az", rr.Scalar(data.az))
+        i += 1
+        if i == 50:
+            break
---
+++ msckf.py
@@ -0,0 +1,6 @@
+from numpy.typing import ArrayLike
+
+
+class MSCKF:
+    def __init__(self, gyro_var: ArrayLike):
+        pass
